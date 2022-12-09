cc1b5eb88e446b504ab461e5c43ccc8e31686faa
kyrolloszakaria <kyrolloszakaria@aucegypt.edu>
2022-12-10 00:03:16 +0200

    Edit: Clk_divider in Digital Clock

--- Digital_Alarm.srcs/sources_1/new/minSecCounter.v
+++ Digital_Alarm.srcs/sources_1/new/minSecCounter.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 module minSecCounter(input clk, reset, output [3:0] secUnits, [3:0] minUnits, [2:0] secTens, [2:0] minTens);
     wire clkOut, secTensCounterEnable, minUnitsCounterEnable, minTensCounterEnable;
-    clockDivider #(50000000) newClk(clk, reset, clkOut);
+    clk_divider #(50000000) newClk(clk, reset, clkOut);
     
     counterModN #(4,10) secUnitsCounter(clkOut, reset, 1, secUnits);
     
